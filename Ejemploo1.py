eaa522ba74315c5df638b21e73838eb6bf41cfd2
lexus210 <73870803@continental.edu.pe>
2024-09-09 18:17:47 -0500

    Refactorización ejem01

--- Ejemploo1.py
+++ Ejemploo1.py
@@ -1,10 +1,10 @@
-x = 10
-y = 5
-z = x + y
+def multiplicacion(multiplicando, multiplicador):
+    producto = multiplicando * multiplicador
+    return producto
 
-def f(a, b):
-    c = a * b
-    return c
 
-resultado = f(x, z)
-print(resultado)
+if __name__ == "__main__":
+    multiplicando = float(input("Multiplicando: "))
+    multiplicador = float(input("Multiplicador: "))
+    resultado = multiplicacion(multiplicando, multiplicador)
+    print(f"{multiplicando} * {multiplicador} = {resultado}")

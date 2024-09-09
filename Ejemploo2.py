0ad96747105c5080ac361cf2e98939df6668dafe
lexus210 <73870803@continental.edu.pe>
2024-09-09 18:32:28 -0500

    Refactorización ejem02

--- Ejemploo2.py
+++ Ejemploo2.py
@@ -1,12 +1,10 @@
-def calcular(a, b, c):
-    res = a * b + c
-    return res
+def calcular(termino01, termino02, termino03):
+    producto = termino01 * termino02 + termino03
+    return producto
 
-def principal():
-    x = 5
-    y = 3
-    z = 7
-    resultado = calcular(x, y, z)
-    print("El resultado es:", resultado)
-
-principal()
+if __name__ == "__main__":
+    termino01 = float(input("Ingrese termino 1: "))
+    termino02 = float(input("Ingrese termino 2: "))
+    termino03 = float(input("Ingrese termino 3: "))
+    resultado = calcular(termino01, termino02, termino03)
+    print(f"{termino01} * {termino02} + {termino03} = {resultado}")
